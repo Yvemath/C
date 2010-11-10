240b672f2a07fb1b81d1b747db8c8c49393ce005
Quo Zi Yvemath <contact@gnamespace.com>
2010-11-10 18:38:12 +0800

    Added some files

---
+++ e4_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+union month{
+  enum short_month[3] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+  enum long_month[30] = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+};
+
+typedef struct date{
+  int day;
+  union month m;
+  int year;
+  int function dob(newdate, newmonth[], newyear){
+    date = newdate;
+    month = newmonth;
+    year = newyear;
+    return 0;
+  }
+}date;
+
+int main(void)
+{
+  char name[30];
+  int ssn;
+  dateofbirth dob;
+}
--- num.c
+++ num.c
@@ -4,9 +4,9 @@ int main(void) {
   int num = 0, count = 0;
   for (count = 0; count < 15; ++count)
     {
-      printf("count = %d",&count);
+      printf("count = %d",count);
       num += count;
-      printf(", num = %d\nHappy?",&num);
+      printf(", num = %d\nHappy?",num);
     }
   return(0);
 }
